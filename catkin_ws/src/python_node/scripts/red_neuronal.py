beec72d753725dc4983cbadbe93697d5e3d4e896
Aaron <picoaaron@gmail.com>
2021-03-30 13:48:59 +0200

    La red se carga, el robot se mueve, pero siempre devuelve hacia delante

--- catkin_ws/src/python_node/scripts/red_neuronal.py
+++ catkin_ws/src/python_node/scripts/red_neuronal.py
@@ -39,25 +39,37 @@
 import rospy
 from std_msgs.msg import String
 
+import roslib
+import sys
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
-from keras.models import load_model
+from keras.models import load_model, model_from_json
+
+import json
+
+import tensorflow
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+
+import Image as im
+
+import geometry_msgs.msg
+from geometry_msgs.msg import Twist
+
+'''
 
 longitud, altura = 32, 32
 modelo = './modelo.h5'
 pesos_modelo = './pesos.h5'
 cnn = load_model(modelo)
 cnn.load_weights(pesos_modelo)
-
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
 
 
 def predict(file):
@@ -76,7 +88,7 @@ def predict(file):
 
   return answer
 
-'''
+
 def main():
   print("Adelante:")
   for i in range(30):
@@ -104,8 +116,137 @@ def main():
   return 0
 '''
 
-if __name__ == '__main__':
+def cnn_model():
+  #
+  # Neural Network Structure
+  #
+  
+  activation = "relu"
+  nb_classes = 3
+
+  input_shape = (32, 32, 3)
+
+  inputs = keras.Input(shape=input_shape)
+  
+  x = layers.Conv2D(52, (5, 5), activation=activation)(inputs)
+  x = layers.MaxPooling2D(pool_size=(4, 4))(x)
+
+  x = layers.Conv2D(124, (5, 5), activation=activation)(x)
+  x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+  x = layers.Flatten()(x)
+
+  x = layers.Dense(120, activation=activation)(x)
+  x = layers.Dense(84, activation=activation)(x)
+  x = layers.Dense(32, activation=activation)(x)
+  
+
+  outputs = layers.Dense(nb_classes, activation='softmax')(x)
+
+  model = keras.Model(inputs=inputs, outputs=outputs)
+
+  return model
+
+
+
+class red_neuronal:
+
+  longitud, altura = 32, 32
+  #modelo = './modelo.json'
+  pesos_modelo = './pesos.h5'
+  model = cnn_model()
+  model.load_weights(pesos_modelo)
+  global graph
+  graph = tensorflow.get_default_graph()
+  global cmd_vel_pub
+
+  
+
+  def __init__(self):
+    #self.image_pub = rospy.Publisher("image_topic_2",Image)
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("robot1/camera/rgb/image_raw", Image, self.callback)
+    self.cmd_vel_pub = rospy.Publisher("/robot1/mobile_base/commands/velocity", Twist)
+    
+    #cnn = load_model(self.modelo)
+    #with open(self.modelo, "r") as f:
+    #  data = json.load(f)
+    #cnn = self.cnn_model()
+    #self.model.load_weights(self.pesos_modelo)
+
+  
+
+  def predict(self, f):
+    '''
+    x = load_img(img, target_size=(self.longitud, self.altura))
+    #x = file.resize((32, 32))
+    #x = file.resize((self.longitud, self.altura))
+    #x = cv2.resize(file, (self.longitud, self.altura))
+    x = img_to_array(x)
+    x = np.expand_dims(x, axis=0)
+    '''
+    img = np.resize(f, (32,32, 3))
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    #keras.backend.clear_session()
+    with graph.as_default():
+      self.model._make_predict_function()
+      array = self.model.predict(x)
+      print(array)
+    
+    result = array[0]
+    answer = np.argmax(result)
+    if answer == 0:
+      print("pred: Adelante")
+    elif answer == 1:
+      print("pred: Derecha")
+    elif answer == 2:
+      print("pred: Izquierda")
+
+    return answer
+
+  def velocidad(self, pred):
+    cmd = geometry_msgs.msg.Twist()
+    cmd.linear.x = cmd.angular.z = 0
+
+    if pred == 0:
+      cmd.linear.x = 0.25
+    elif pred == 1:
+      base_cmd.angular.z = 0.75
+      base_cmd.linear.x = 0.25
+    elif pred == 2:
+      base_cmd.angular.z = -0.75
+      base_cmd.linear.x = 0.25
+
+    return cmd
+      
+
+
+
+  def callback(self, image_message):
     try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+      #Obtenemos la imagen
+      cv_image = self.bridge.imgmsg_to_cv2(image_message, desired_encoding='bgr8')
+      
+      #La red decide que movimiento hacer
+      pred = self.predict(cv_image)
+
+      #Calculamos la velocidad que vamos a enviar al robot
+      base_cmd = self.velocidad(pred)
+
+      #Enviamos la velocidad
+      self.cmd_vel_pub.publish(base_cmd)
+
+    except CvBridgeError as e:
+      print(e)
+
+def main(args):
+  red = red_neuronal()
+  rospy.init_node('red_neuronal', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+
+if __name__ == '__main__':
+  main(sys.argv)
